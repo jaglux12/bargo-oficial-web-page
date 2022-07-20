5c08aa3944b1b5449c5580cb4a9e379798cbf0e4
jaglux12 <gonzalopez239@gmail.com>
2022-07-20 10:52:46 -0300

    agregamos php a la pagina principal

---
+++ requieres/index/body.php
@@ -0,0 +1,80 @@
+<section>
+
+  <article>
+
+    <div class="container-fluid px-3">
+
+      <div id="carouselExampleSlidesOnly" class="carousel slide d-xs-block d-sm-none" data-bs-ride="carousel">
+
+        <div class="carousel-inner">
+
+          <div class="carousel-item active rounded-4 py-5" id="panelCarousel">
+            <div class="row">
+              <div class="row align-items-center justify-content-center ms-2">
+                  <img src="imgs/books/index/carousel-img-1.png" class="d-block w-75">
+              </div>
+              <div class="row">
+                  <label class="h1 text-center text-muted display-4 ms-2" id="tituloPaneles">
+                    Descubre nuevas<br/>
+                    <label class="display-3 ms-2" id="subTituloPaneles"> <strong> Historias </strong> </label>
+                  </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item rounded-4 py-5" id="panelCarousel">
+            <div class="row">
+              <div class="row align-items-center justify-content-center ms-2">
+                  <img src="imgs/books/index/carousel-img-2.png" class="d-block w-75">
+              </div>
+              <div class="row">
+                  <label class="h1 text-center text-muted display-4 ms-2" id="tituloPaneles">
+                    Comparte tus lecturas<br/>
+                    <label class="display-3 ms-2" id="subTituloPaneles"> <strong> con los demás </strong> </label>
+                  </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item rounded-4 py-5" id="panelCarousel">
+            <div class="row">
+              <div class="row align-items-center justify-content-center ms-4">
+                  <img src="imgs/books/index/carousel-img-3.png" class="d-block w-50">
+              </div>
+              <div class="row">
+                  <label class="h1 text-center text-muted display-4 ms-2" id="tituloPaneles">
+                    Lleva tu libro<br/>
+                    <label class="display-3 ms-2" id="subTituloPaneles"> <strong> hacia la cima </strong> </label>
+                  </label>
+              </div>
+            </div>
+          </div>
+
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Siguiente</span>
+        </button>
+
+        </div>
+
+     </div>
+
+    </div>
+
+  </article>
+
+  <article class="container-fluid px-3 py-5  mt-4 d-flex flex-column justify-content-center align-items-center d-xs-block d-sm-none ">
+
+    <h1 class="fs-1 mt-5" id="tituloPaneles" style="color: #ff8200;">Sobre</h1>
+    <label class="fs-4 text-center px-3" id="tituloPaneles">Lleva tus lecturas a cualquier lugar, </label>
+    <label class="fs-4 text-center px-3" id="tituloPaneles">comparte, escribe reseñas y califica.</label>
+    <label class="fs-4 mb-5" id="tituloPaneles" style="color:#ff8200;">Sé tu escritor favorito.</label>
+
+  </article>
+
+</section>
