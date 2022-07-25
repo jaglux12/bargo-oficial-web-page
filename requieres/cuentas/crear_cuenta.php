d54527f55fb1fd8840a9c9f963869d70fe5a1808
jaglux12 <gonzalopez239@gmail.com>
2022-07-25 17:50:06 -0300

    agregamos cambios en la pagina para agregar usuarios

---
+++ requieres/cuentas/crear_cuenta.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap - CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+    crossorigin="anonymous">
+
+    <!-- Bootstrap - Iconos -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- CSS locales -->
+    <link href="../../style/create_account/style-createAccount.css" rel="stylesheet">
+    <link href="../../style/index/style-index.css" rel="stylesheet">
+
+    <title>Bargo - Crear cuenta</title>
+
+  </head>
+
+  <body>
+
+
+    <!-- Panel de crear cuenta -->
+    <header>
+      <?php
+        require 'panel_crearCuenta.php'
+       ?>
+    </header>
+
+    <!-- JQuery -->
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.js"
+      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+      crossorigin="anonymous">
+    </script>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous">
+    </script>
+
+  </body>
+
+</html>
---
+++ requieres/cuentas/panel_crearCuenta.php
@@ -0,0 +1,49 @@
+<div class="row m-0 vh-100 px-5 justify-content-center align-items-center" id="panelPrincipal">
+
+  <div class="col-12 col-md-6 py-5 px-4 d-xs-block rounded d-sm-none" id="panelCrearCuenta">
+
+    <form class="px-2" action="index.html" method="post">
+
+      <div class="d-flex justify-content-center">
+        <h2 id="tituloBargo" class="text-light fs-1">Crear cuenta</h2>
+      </div>
+
+      <div class="row mt-4 px-2">
+          <input type="username" class="form-control bg-transparent border-light rounded-1"placeholder="usuario">
+      </div>
+
+      <div class="row mt-3 px-2">
+          <input type="password" class="form-control bg-transparent border-light rounded-1" placeholder="contraseña">
+      </div>
+
+      <div class="mt-4 d-flex justify-content-center">
+          <a href="#" id="yatengounacuenta"> ¿Ya tienes una cuenta? </a>
+      </div>
+
+      <div class="mt-4 d-flex justify-content-center">
+          <a href="#">
+            <button type="button" name="button" class="btn btn-dark p-3 rounded-pill px-4">Iniciar sesión</button>
+          </a>
+      </div>
+
+    </form>
+
+  </div>
+
+  <div class="col-md-6 d-none d-md-block bg-light pt-5 pb-4 px-4 h-50">
+    <div class="d-flex justify-content-center flex-column">
+      <figure>
+        <blockquote class="blockquote">
+          Siempre hay que tratar de ser el mejor, pero nunca creerse el mejor.
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Juan Manuel Fangio
+        </figcaption>
+      </figure>
+    </div>
+    <div class="d-flex justify-content-center align-items-end h-50">
+      <h1 id="tituloBargo">Bargo</h1>
+    </div>
+  </div>
+
+</div>
