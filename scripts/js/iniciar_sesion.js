24489fdf083117d304eb7a44d98b40381d806326
Jaglux12 <83303348+jaglux12@users.noreply.github.com>
2022-08-16 17:15:15 -0300

    borrando archivos innecesarios

--- scripts/js/iniciar_sesion.js
+++
@@ -1,8 +0,0 @@
-function saludo(){
-  alert("ijueputa");
-}
-
-window.onload = function(){
-  var texto = document.getElementById("boton_saludooo")
-  texto.addEventListener("click",saludo,false);
-}
